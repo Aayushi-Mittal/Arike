ff26f5de001e799c1fd5333165e317fb0071f5e4
Aayushi Mittal <aayushimittal088@gmail.com>
2022-02-28 22:44:50 +0530

    Installed django-tailwind

--- arike/settings.py
+++ arike/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+# MIDDLEWARE = [
+#   "django_browser_reload.middleware.BrowserReloadMiddleware"
+# ]
