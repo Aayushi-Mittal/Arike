4094db7353a572b209d9dba23f42801377221a85
Aayushi Mittal <aayushimittal088@gmail.com>
2022-03-07 04:17:55 +0530

    Temp code - broken

--- arike/urls.py
+++ arike/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from arike_manager.views import login, shome, thome
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("user/login/", login),
+    path("shome/", shome),
+    path("thome/", thome),
+    # path("home/", login),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
